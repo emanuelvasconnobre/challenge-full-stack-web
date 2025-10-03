77cf7e53d102e194ddef30a6cce99e9e78f4a61e
emanuelvasconnobre <emanuelvanobre@gmail.com>
2025-10-03 16:34:30 -0300

    feat(husky): adding husky configuration and pre-commit check process

--- api/jest.unit.config.js
+++ api/jest.unit.config.js
@@ -1,3 +1,7 @@
+const { createDefaultPreset } = require("ts-jest");
+
+const tsJestTransformCfg = createDefaultPreset().transform;
+
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
   testEnvironment: "node",
