c4c564fbad370ce57ac35e2be93291296e0caf6e
emanuelvasconnobre <emanuelvanobre@gmail.com>
2025-10-07 15:17:07 -0300

    feat(frontend): adding web application structure using vuetify

---
+++ web/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-app>
+    <Sidebar />
+
+    <v-main class="pa-4">
+      <router-view />
+    </v-main>
+  </v-app>
+</template>
+
+<script setup lang="ts">
+import Sidebar from '@/components/Sidebar.vue'
+</script>
+
---
+++ web/src/components/SideBar.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-navigation-drawer app>
+    <v-list v-for="link in links" :key="link.to">
+      <v-list-item
+        :to="link.to"
+        :title="link.title"
+        link
+        :prepend-icon="link.icon"
+        :aria-label="link.description"
+        role="link"
+      />
+    </v-list>
+  </v-navigation-drawer>
+</template>
+
+<script setup lang="ts">
+const links = [
+  {
+    to: "/",
+    icon: "mdi-text-box-outline",
+    description: "Go to home page.",
+    title: "Home",
+  },
+  {
+    to: "students",
+    icon: "mdi-star-circle-outline",
+    description: "Go to students page.",
+    title: "Students",
+  },
+];
+</script>
